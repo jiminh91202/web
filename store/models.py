68cda2a6d50433f3f2f1f5dd4221c08104b6ef9a
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-11-11 15:50:17 +0700

    update

---
+++ store/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Manufacturer(models.Model):
+    ManufacturerID = models.AutoField(primary_key=True)
+    ManufacturerName = models.CharField(max_length=255)
+    ContactInfo = models.TextField()
+    class Meta:
+        verbose_name_plural = 'manufacturers'
+    def __str__(self):
+        return self.name
+    
+
+class Products(models.Model):
+    ProductID = models.AutoField(primary_key=True)
+    ProductName = models.CharField(max_length=255)
+    Description = models.TextField()
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    QuantityInStock = models.IntegerField()
+    Image = models.ImageField(upload_to='images/')
+    Specifications = models.TextField()
+    ManufacturerID = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    YearManufactured = models.DateField()
+    class Meta:
+        verbose_name_plural = 'product'
+    def __str__(self):
+        return self.title
