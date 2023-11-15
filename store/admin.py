de195e48f8144a44b1b466c64e432506dd870878
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-11-16 06:06:16 +0700

    update

--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Manufacturer,Products
-
-@admin.register(Manufacturer)
-class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ['ManufacturerID','ManufacturerName','ContactInfo']
-
-@admin.register(Products)
-class ProductsAdmin(admin.ModelAdmin):
-    list_display = ['ProductID','ProductName','Description']
-    
--- store/models.py
+++ store/models.py
@@ -1,24 +1,3 @@
-# Create your models here.
 from django.db import models
 
-class Manufacturer(models.Model):
-    ManufacturerID = models.AutoField(primary_key=True)
-    ManufacturerName = models.CharField(max_length=255)
-    ContactInfo = models.TextField()
-    class Meta:
-        verbose_name_plural = 'manufacturers'
-    
-    
-
-class Products(models.Model):
-    ProductID = models.AutoField(primary_key=True)
-    ProductName = models.CharField(max_length=255)
-    Description = models.TextField()
-    Price = models.DecimalField(max_digits=10, decimal_places=2)
-    QuantityInStock = models.IntegerField()
-    Image = models.ImageField(upload_to='images/')
-    Specifications = models.TextField()
-    ManufacturerID = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    YearManufactured = models.DateField()
-    class Meta:
-        verbose_name_plural = 'product'
+# Create your models here.
