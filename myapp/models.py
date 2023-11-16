74b6f99dd023195405dcb557ccbe2066ed8f7acf
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-11-16 21:32:14 +0700

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,24 +1,23 @@
 from django.db import models
 # Create your models here.
-
-from django.db import models
-
 class Brand(models.Model):
     name = models.CharField(max_length=255)
+    ID = models.AutoField(primary_key=True)
     def __str__(self):
         return self.name
     
 
 class Motorbike(models.Model):
+    ID = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=14, decimal_places=2)
-
     def __str__(self):
         return self.name
 
 
 class Customer(models.Model):
+    ID = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
@@ -27,10 +26,10 @@ class Customer(models.Model):
     
 
 class Order(models.Model):
+    ID = models.AutoField(primary_key=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     motorbike = models.ForeignKey(Motorbike, on_delete=models.CASCADE)
     order_date = models.DateField()
-
     def __str__(self):
         return f"Order {self.id}: {self.motorbike.name} for {self.customer.name} on {self.order_date}"
 
