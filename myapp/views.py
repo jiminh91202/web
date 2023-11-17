891d8fcedf4edf8e871f1d6523822adf87f03dd8
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-11-17 13:03:02 +0700

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
-=======
 from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
->>>>>>> Naolia_v3
 
 # Create your views here.
 def shop(request):
@@ -25,8 +19,6 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
-<<<<<<< HEAD
-=======
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
@@ -37,7 +29,6 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
->>>>>>> Naolia_v3
 
 def logout_view(request):
     if request.method == 'POST':
