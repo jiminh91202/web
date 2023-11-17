5f34c2e14c11119f14b719b74c64ee01d9e67a65
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-11-17 12:01:22 +0700

    login and register

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 class Brand(models.Model):
     name = models.CharField(max_length=255)
@@ -14,7 +16,7 @@ class Motorbike(models.Model):
     def __str__(self):
         return self.name
 
-class Customer(models.Model):
+class Customer(AbstractUser):
     ID = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     email = models.EmailField()
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def shop(request):
     template = loader.get_template('shop.html')
     return HttpResponse(template.render())
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('login')
