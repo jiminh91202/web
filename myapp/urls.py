2aa43b7eaa6a6d54302e91df8b66d86c64ae904c
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-11-19 16:25:29 +0700

    update signin

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 
@@ -17,5 +15,6 @@ urlpatterns = [
     path('about/',views.about,name="about"),
     path('news/',views.news,name="news"),
     path('shop/details/<int:id>', views.details, name='details'),
+
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,10 @@
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect 
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import *
-from .forms import RegistrationForm
+from .forms import *
+from django.contrib.auth import authenticate, login as auth_login
 
 
 # Create your views here.
@@ -21,10 +23,6 @@ def contact(request):
 
 def homepage(request):
     template = loader.get_template('homepage.html')
-    return HttpResponse(template.render())
-
-def login(request):
-    template = loader.get_template('login.html')
     return HttpResponse(template.render())
 
 def register(request):
@@ -56,3 +54,17 @@ def register(request):
             form.save()
             return HttpResponseRedirect('/')
     return render(request, 'register.html', {'form': form})
+
+def login(request):  
+    form = LoginForm()
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('homepage')
+        else:
+            return redirect('login')
+    else:
+        return render(request,'login.html',{'form' : form })
