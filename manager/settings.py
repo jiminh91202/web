4473e9fc221e65dbe099e32371f5e2df86b0a381
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-11-21 00:39:22 +0700

    update

--- manager/settings.py
+++ manager/settings.py
@@ -135,6 +135,6 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'product')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = ''
