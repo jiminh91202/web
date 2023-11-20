7e81756fc6df5a8f176332d8d283c4ea6f40f4b6
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-11-21 01:18:53 +0700

    update

--- manager/settings.py
+++ manager/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(3a6y809%vs#@98g+k_%zfdaq-)(@+su65qr&uud9)4u2*9v_9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -123,18 +123,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'productionfiles'
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'myapp', 'static'),
-)
+STATICFILES_DIRS = [
+    BASE_DIR / 'myapp',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'product')
+MEDIA_ROOT = os.path.join(BASE_DIR,'product')
 
 MEDIA_URL = ''
